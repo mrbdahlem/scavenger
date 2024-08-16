a9a2ba57313b8de460bbfb9c9436d29bfd41ab47
Brian Dahlem <dahlem.brian@cusd80.com>
2024-08-16 12:59:43 -0700

    first commit

---
+++ src/main/java/run/mycode/scavanger/ScavangerApplication.java
@@ -0,0 +1,13 @@
+package run.mycode.scavanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScavangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScavangerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/run/mycode/scavanger/ScavangerApplicationTests.java
@@ -0,0 +1,13 @@
+package run.mycode.scavanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScavangerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

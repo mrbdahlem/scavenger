90edb9dab5bff9f12c6266c5556389d3eea6e589
Brian Dahlem <dahlem.brian@cusd80.com>
2024-08-21 19:55:07 +0000

    maybe?

--- builddb.sh
+++ builddb.sh
@@ -1,2 +1,9 @@
+#!/bin/bash
+
+if ! pgrep -x "mariadb" > /dev/null
+then
+    sudo /etc/init.d/mariadb start
+fi
+
 sudo mariadb -Bse "CREATE DATABASE $DATABASE_NAME; GRANT ALL PRIVILEGES ON $DATABASE_NAME.* to '$DATABASE_USER'@'localhost' IDENTIFIED BY '$DATABASE_PW';"
 

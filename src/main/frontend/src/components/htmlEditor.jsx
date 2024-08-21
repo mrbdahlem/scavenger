f5368a6c0c01466b2b3a9aca71e99eadd898405c
mrbdahlem <dahlem.brian@cusd80.com>
2024-08-20 23:15:30 -0700

    config the jodit editor

--- src/main/frontend/src/components/htmlEditor.jsx
+++ src/main/frontend/src/components/htmlEditor.jsx
@@ -27,7 +27,7 @@ export const Editor = ({ initialContent }) => {
             config={config}
             tabIndex={1} // tabIndex of textarea
             onBlur={newContent => setContent(newContent)} // preferred to use only this option to update the content for performance reasons
-            onChange={newContent => {}}
+            // onChange={newContent => {}}
         />
     );
 };

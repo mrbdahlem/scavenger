140f57300339b48ab76b27059ef85b1366fc200d
Brian Dahlem <dahlem.brian@cusd80.com>
2024-08-22 20:02:42 +0000

    edit task descriptions!

--- src/main/frontend/src/components/taskCard.jsx
+++ src/main/frontend/src/components/taskCard.jsx
@@ -2,24 +2,46 @@ import {useState} from "react";
 
 import {Card, CardTitle, CardHeader, CardFooter, CardContent, CardDescription} from "@/components/ui/card";
 import {Button} from "@/components/ui/button";
+import {Input} from "@/components/ui/input";
+import Editor from '@/components/htmlEditor.jsx';
 
 export const TaskCard = ({taskName, taskDescription, onChange}) => {
     console.log(taskName);
     console.log(taskDescription);
     const [name, setName] = useState(taskName);
     const [description, setDescription] = useState(taskDescription);
+    const [editing, setEditing] = useState(false);
+
+    function saveTask() {
+        if (onChange) {
+            onChange({name, description});
+        }
+        setEditing(false)
+    }
 
     return (
-        <Card>
-            <CardHeader>
-                <CardTitle>{name}</CardTitle>
-                &nbsp;<Button>✏</Button>
+        <Card className="mt-1">
+            <CardHeader className="flex flex-row items-center justify-between p-3">
+                <CardTitle className="text-sm font-bold w-full">
+                    {(!editing && name)
+                        || <>
+                        <Input type="text" value={name} onChange={(e) => setName(e.target.value)} /> 
+                        <p className="font-light italic text-xs">(participants will not see the task name until they complete the task)</p></>
+                    }
+
+                </CardTitle>
+                { (!editing && <Button variant="ghost" onClick={()=>setEditing(true)}>✏</Button>)
+                 ||            <Button variant="ghost" onClick={saveTask}>✔</Button> }
+
             </CardHeader>
-            <CardContent>
-                <CardDescription>{description}</CardDescription>
+            <CardContent className="px-3 py-0">       
+                { ( !editing &&          
+                    <CardDescription className="truncate" dangerouslySetInnerHTML={{__html: description}} /> ) 
+                    || <Editor value={description} onChange={setDescription} 
+                        placeholder="Enter the description of the task..."/> }
             </CardContent>
             
-            <CardFooter>
+            <CardFooter className="p-3">
             </CardFooter>
         </Card>
 
--- src/main/frontend/src/components/taskList.jsx
+++ src/main/frontend/src/components/taskList.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import {gameService} from "@/lib/service/game.service.js";
 
@@ -6,22 +6,23 @@ import { Button } from "@/components/ui/button";
 import { TaskCard } from "./taskCard";
 
 export const TaskList = ({gameId, tasks, onChange}) => {
-    const [taskList, setTaskList] = useState(tasks);
+    const [taskList, setTaskList] = useState([]);
+    useEffect(()=>{
+        setTaskList(tasks);
+    }, [tasks])
 
     const addTask = () => {
         gameService.addTask(gameId).then(data => {
-            //setTaskList([...taskList, {id: data.id, name: data.title, description: data.description}]);
-            onChange([...taskList, {id: data.id, name: data.title, description: data.description}]);
-            console.log(data);
+            //setTaskList([...taskList, {id: data.id, name: data.name, description: data.description}]);
+            onChange([...taskList, {id: data.id, name: data.name, description: data.description}]);
         });
     };
 
     return (
         <div className="mt-5">
             <Button onClick={addTask}>+ Create New Task</Button>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div className="flex flex-col gap-4 mt-2">
                 {taskList.map((task) => (
-                    console.log("Task", task),
                     <TaskCard key={task.id} taskName={task.name} taskDescription={task.description} />
                 ))}
             </div>

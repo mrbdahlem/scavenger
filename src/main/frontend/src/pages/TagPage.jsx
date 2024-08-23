f56430423176427afabae49817496316be85e6f8
mrbdahlem <dahlem.brian@cusd80.com>
2024-08-22 21:27:23 -0700

    Starting tag handling

--- src/main/frontend/src/pages/TagPage.jsx
+++ src/main/frontend/src/pages/TagPage.jsx
@@ -6,6 +6,11 @@ export const TagPage = () => {
         <div>
             <h1>Tags {id}</h1>
             <p>Coming soon...</p>
+            <!-- If logged in - if unclaimed assign to game and task
+                              - if claimed unassign confirm then assign only if user owns associated game
+                 If not logged in - if claimed and game is unlocked, check if playing, add checkpoint
+                                  - if unclaimed, game is locked, or not playing, give error msg
+              -->
         </div>
     )
 }

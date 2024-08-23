870f25b11763361600a010c1171c837fef6238e8
Brian Dahlem <dahlem.brian@cusd80.com>
2024-08-23 21:36:20 +0000

    Checking if tag is claimed

--- src/main/frontend/src/pages/TagPage.jsx
+++ src/main/frontend/src/pages/TagPage.jsx
@@ -1,27 +1,40 @@
 import {useState, useEffect} from "react";
 import {useParams} from "react-router-dom";
 import tagService from "@/lib/service/tag.service.js";
+import {useAuth} from "@/hooks/useAuth";
+import { Tag } from "lucide-react";
 
 export const TagPage = () => {
     const { hash } = useParams();
+    const { user } = useAuth();
+    const [tag, setTag] = useState(null);
 
     useEffect(() => {
-        console.log(hash);
-        const tag = tagService.loadTag(hash);
-        console.log(tag);
+        setTag(tagService.loadTag(hash));
     }, [hash])
 
-    return (
-        <div>
-            <h1>Tags { hash }</h1>
-            <p>Coming soon...</p>
-            {/* If logged in - if unclaimed assign to game and task
-                             - if claimed unassign confirm then assign only if user owns associated game
-                If not logged in - if claimed and game is unlocked, and cookie for playing, add checkpoint
-                                 - if unclaimed, game is locked, or not playing, give error msg
-              */}
-        </div>
-    )
+    if (user) {
+        return (
+            <div>
+                {/* If logged in - if unclaimed assign to game and task
+                                - if claimed unassign confirm then assign only if user owns associated game
+                    If not logged in - if claimed and game is unlocked, and cookie for playing, add checkpoint
+                                    - if unclaimed, game is locked, or not playing, give error msg
+                */}
+                <h1>Tag { hash }</h1>
+
+                {tag && !tag.game && <p>Unclaimed Tag</p>}
+            </div>
+        )
+    }
+    else {
+        return (
+            <div>
+                <h1>Tags { hash }</h1>
+                <p>Coming soon...</p>
+            </div>
+        )
+    }
 }
 
 export default TagPage;

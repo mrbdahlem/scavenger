8e9b0ead7137cb1339453f5c1f22dd773055a8ad
Brian Dahlem <dahlem.brian@cusd80.com>
2024-12-11 17:47:39 +0000

    fixed #1

--- src/main/frontend/src/components/playInfo.jsx
+++ src/main/frontend/src/components/playInfo.jsx
@@ -8,7 +8,7 @@ export const PlayInfo = ({data}) => {
         <div className="border rounded border-black p-3">
             <div className="flex flex-row items-center justify-between gap-3">
                 <h2 className="text-lg font-bold">Player: {data.name}</h2>
-                <div>Started: {new Date(data.startTime).toLocaleString()}</div>
+                <div>Started: {new Date(data.startTime + 'Z').toLocaleString()}</div>
             </div>
             <Progress value={data.percentComplete} />
         </div>
